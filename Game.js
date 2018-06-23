5476b7d326dca781dcbf1fa1c3ae34652e0e28fe
Randy L <randy.loffelmacher@vroom.com>
2018-06-23 13:24:09 -0400

    wip this is now giving positive num at least!

--- Game.js
+++ Game.js
@@ -3,23 +3,24 @@ export default class Game {
 		this._frames = [];
 		
 		for (let ii=0;ii<10;ii++) {
-			this._frames[ii] = new Frame();
+			this._frames[ii] = new Frame(this);
 		}
-		this._frames[9] = new TenthFrame();
-		this._turn = 1;
+		this._frames[9] = new TenthFrame(this);
+		this.currentFrame = 0;
 		this._score = 0;
 	}
 	roll(pins) {
-		if (this._turn <= 10) {
-			this._frames[this._turn-1].roll(pins);
-			this._turn++;
+		console.log(`entering Game.roll() frame index is: ${this.currentFrame}`);
+		// turn starts at 1, human-readable
+		if (this.currentFrame < 10) {
+			this._frames[this.currentFrame].roll(pins);
+			// TODO: replace naive impl with on using map
 		} 
 	}
 	score() {
 		let retval = 0;
 		this._frames.forEach((ff) => {
-			// console.log('key: ', k);
-			// console.log('val: ', v);
+			// console.log(`the score is: ${ff.score()}`);
 			retval += ff.score();
 		});
 		return retval;
@@ -27,30 +28,48 @@ export default class Game {
 }
 
 class Frame {
-	constructor() {
+	constructor(game) {
 		this._rollOne = -1;
 		this._rollTwo = -1;
+		this._game = game;
 	}
 	roll(pins) {
-		if (this._rollOne != -1) {
+		if (this._rollOne == -1) {
+			console.log('setting pins for rollOne');
 			this._rollOne = pins;
-		} else if (this._rollOne != 10 && this._rollTwo != -1) {
+		} else if (this._rollOne != 10 && this._rollTwo == -1) {
+			console.log('setting pins for rollTwo');
 			this._rollTwo = pins;
+			// console.log(`game is: ${this._game}`);
+			this._game.currentFrame += 1;
+		} else if (this._rollOne == 10) { 
+			this._rollTwo = null;
 		} else {
-			this._nextFrame = -1; // not right!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			console.log('eleseeeeeeee');
 		}
 	}
 	score() {
-		return this._rollOne + this._rollTwo;
+		// console.log(`what is: {this._rollOne + this._rollTwo`)
+		return this._rollOne + (null ? 0 : this._rollTwo);
 	}
 }
 
 class TenthFrame extends Frame {
-	constructor() {
-		super();
+	constructor(game) {
+		super(game);
 		this._rollThree = null;
 	}
+	roll(pins) {
+		if (this._rollThree == -1) {
+			this._rollThree = pins;
+			this._game.currentFrame += 1;
+		}
+		// TODO: below is printing twice WHY/////??????
+		console.log('INSIDE TENTHFRAME ROLL');
+
+	}
 	score() {
-		return 0;
+		// console.log('INSIDE TENTHFRAME SCORE!');
+		return this._rollOne + this._rollTwo + this._rollThree;
 	}
 }
