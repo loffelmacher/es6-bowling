ba182b07b52052b5a850eae3e21d1b100b1cd383
Randy L <randy.loffelmacher@vroom.com>
2018-06-24 12:19:19 -0400

    refactor

--- Game.js
+++ Game.js
@@ -13,18 +13,17 @@ export default class Game {
 	score() {
 		let score = 0;
 		let frameIndex = 0;
-		for (let frame=0; frame<21; frame+=2) {
-			let thisRoll = this._rolls[frame];
-			let nextRoll = this._rolls[frame+1];
-			if (thisRoll + nextRoll == 10) { // spare
-				score += 10 + ((frame+2 <= 21) ? this._rolls[frame+2] : 0);
+		for (let frame=0; frame<10; frame++) {
+			if (this._isSpare(frameIndex)) {
+				score += 10 + ((frameIndex+2 <= 21) ? this._rolls[frameIndex+2] : 0);
 			} else {
-				score += thisRoll + nextRoll;
+				score += this._rolls[frameIndex] + this._rolls[frameIndex+1];
 			}
+			frameIndex += 2;
 		}
 		return score;
 	}
-	_isSpare() {
-
+	_isSpare(frameIndex) {
+		return this._rolls[frameIndex] + this._rolls[frameIndex+1] == 10;
 	}
 }
