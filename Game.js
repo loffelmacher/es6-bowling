5c6029f835d8d84bff07dfb432a6a5616173d46f
Randy L <randy.loffelmacher@vroom.com>
2018-06-24 11:55:25 -0400

    cleanup

--- Game.js
+++ Game.js
@@ -12,15 +12,19 @@ export default class Game {
 	}
 	score() {
 		let score = 0;
-		for (let ii=0; ii<21; ii+=2) {
-			let xx = this._rolls[ii];
-			let yy = this._rolls[ii+1];
-			if (xx + yy == 10) { // spare
-				score += 10 + ((ii+2 <= 21) ? this._rolls[ii+2] : 0);
+		let frameIndex = 0;
+		for (let frame=0; frame<21; frame+=2) {
+			let thisRoll = this._rolls[frame];
+			let nextRoll = this._rolls[frame+1];
+			if (thisRoll + nextRoll == 10) { // spare
+				score += 10 + ((frame+2 <= 21) ? this._rolls[frame+2] : 0);
 			} else {
-				score += xx + yy;
+				score += thisRoll + nextRoll;
 			}
 		}
 		return score;
+	}
+	_isSpare() {
+
 	}
 }
--- test/bowling-spec.js
+++ test/bowling-spec.js
@@ -37,52 +37,11 @@ lab.test('Test One Spare', (done) => {
   done();
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const gg = new Game();
-
-// describe('game:', function() {
-//   describe('foo', function(cb) {
-//     it('bar', function() {
-//       cb();
-//     });
-//   });
-// });
-
-
-// describe('game:', function() {
-//   describe('foo', function() {
-//     it('bar', function() {
-//       return true;
-
-//       // return api.get(apiPath + '/vin/5N1AN08U08C516104')
-//       //   .expect(200)
-//       //   .then(function({body}){
-//       //     expect(body).to.contain(['success', 'data']);
-//       //     expect(body.success).to.be.true();
-//       //     expect(body.data).to.contain(['basicData']);
-//       //     expect(body.data.basicData).to.contain(['year', 'make', 'model']);
-//       //     expect(body.data.basicData.year).to.be.equal(2008);
-//       //     expect(body.data.basicData.make.toLowerCase()).to.be.equal('nissan');
-//       //     expect(body.data.basicData.model.toLowerCase()).to.contain('xterra');
-//       //     expect(body.data.basicData.source.toLowerCase()).to.contain(helpers.NADA.toLowerCase());
-//       //   });
-//     });
-//   });
-// });
+lab.test('Test One Strike', (done) => {
+  gg.roll(10);
+  gg.roll(3);
+  gg.roll(4);
+  rollMany(16, 0);
+  expect(gg.score()).to.equal(24);
+  done();
+});
